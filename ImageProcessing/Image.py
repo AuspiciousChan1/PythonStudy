1aaa0222eaf2e1d7d3284bf004bade37bf13331b
AuspiciousChan <AuspiciousChan@qq.com>
2018-01-31 23:38:54 +0800

    testing k-means in sklearn

--- ImageProcessing/Image.py
+++ ImageProcessing/Image.py
@@ -17,7 +17,7 @@ class Image:
         self.__length = len(self.__image[:, 0, 0])
         self.__width = len(self.__image[0, :, 0])
 
-    def __out_of_range(self, x: int, y: int)-> bool:
+    def __out_of_range(self, x: int, y: int) -> bool:
         return x >= self.__length or x < 0 or y >= self.__width or y < 0
 
     def show(self):
@@ -28,37 +28,37 @@ class Image:
     def has_alpha(self):
         return True if len(self.__image[0, 0, :]) == 4 else False
 
-    def length(self)-> int:
+    def length(self) -> int:
         return self.__length
 
-    def width(self)->int:
+    def width(self) -> int:
         return self.__width
 
-    def rgba_array(self)-> numpy.ndarray:
-        if len(self.__image[0, 0, :]) >= 4:
+    def rgba_array(self) -> numpy.ndarray:
+        if self.has_alpha():
             return self.__image
         else:
             rgb = self.__image
             alpha = numpy.ones([self.length(), self.width(), 1])
             return numpy.concatenate((rgb, alpha), axis=2)
 
-    def rgb_array(self)-> numpy:
+    def rgb_array(self) -> numpy:
         return self.__image[:, :, 0:3]
 
-    def red_array(self)-> numpy.ndarray:
+    def red_array(self) -> numpy.ndarray:
         return self.__image[:, :, 0]
 
-    def green_array(self)-> numpy.ndarray:
+    def green_array(self) -> numpy.ndarray:
         return self.__image[:, :, 1]
 
-    def blue_array(self)-> numpy.ndarray:
+    def blue_array(self) -> numpy.ndarray:
         return self.__image[:, :, 2]
 
-    def alpha_array(self)-> numpy.ndarray:
+    def alpha_array(self) -> numpy.ndarray:
         return self.__image[:, :, 3] if self.has_alpha() else numpy.array([])
 
     # 颜色的角相似度
-    def color_angle_similarity(self, x0: int, y0: int, x1: int, y1: int)-> float:
+    def color_angle_similarity(self, x0: int, y0: int, x1: int, y1: int) -> float:
         if self.__out_of_range(x0, y0) or self.__out_of_range(x1, y1):
             print("out of range")
             return -1
@@ -67,19 +67,24 @@ class Image:
         return vector.cosine(v0, v1)
 
     # 红绿蓝占比
-    def rgb_proportion(self, x: int, y: int)->tuple:
+    def rgb_proportion(self, x: int, y: int) -> tuple:
         if self.__out_of_range(x, y):
             print("out of range")
-            return -1
+            return -1, -1, -1
         return vector.normalize(self.__image[x, y, 0: 3])
 
-    def red_proportion(self, x: int, y: int)->float:
+    def red_proportion(self, x: int, y: int) -> float:
         return self.rgb_proportion(x, y)[0]
 
-    def green_proportion(self, x: int, y: int)->float:
+    def green_proportion(self, x: int, y: int) -> float:
         return self.rgb_proportion(x, y)[1]
 
-    def blue_proportion(self, x: int, y: int)->float:
+    def blue_proportion(self, x: int, y: int) -> float:
         return self.rgb_proportion(x, y)[2]
 
-
+    def normalize_rgb_array(self):
+        img = numpy.reshape(self.__image, (-1, 4))
+        color_array = img[:, 0: 3]
+        for i in range(len(color_array[:])):
+            color_array[i] = vector.normalize(color_array[i])
+        return color_array
--- Main.py
+++ Main.py
@@ -2,16 +2,37 @@ import json
 import os
 
 import pymongo
-import pymongo as pymongo
 from pymongo import MongoClient
 from matplotlib import image, pyplot
 import numpy
 import scipy
+from sklearn import cluster, externals
 
 from ImageProcessing import Image
 from Clusters import MyCluster
 
 img = Image.Image("Sources/Left.png")
+# print("img: \n", img.rgb_array())
+img_len = img.length()
+img_wid = img.width()
+g_arr = numpy.resize(img.green_array(), (img_len, img_wid, 1))
+b_arr = numpy.resize(img.blue_array(), (img_len, img_wid, 1))
+data = numpy.concatenate((g_arr, b_arr), axis=2)
+# data = data1[img_len/4: img_len*3/4, :]#这一排希望把没用的上四分之一和下四分之一砍掉
+data = numpy.reshape(data, (-1, 2))
+# print("data:\n", data)
+clu = cluster.MiniBatchKMeans(n_clusters=16, max_iter=len(data[:, :]))
+result = clu.fit(data)
+img_res = numpy.reshape(result.labels_, (img_len, img_wid))
+print(img_res)
+# mark = ['sk', 'gray', 'brown', 'sr', 'chocolate', 'tan', 'sy', 'sg', 'slime', 'sc', 'cyan', 'navy', 'sb', 'sm']
+mark = ['sk', 'gray', 'brown', 'sr', 'chocolate', 'tan', 'darkgoldenrod', 'sy', 'sg', 'sc', 'cyan', 'navy', 'sb', 'indigo', 'sm', 'pink']
+for i in numpy.linspace(0, img_len - 1, 100):
+    for j in numpy.linspace(0, img_wid - 1, 50):
+        try:
+            pyplot.plot(int(j), -int(i), mark[img_res[int(i), int(j)]])
+        except ValueError:
+            print(mark[img_res[int(i), int(j)]])
 
-
-
+pyplot.axis("equal")
+pyplot.show()
