84919dd717a751df460a1cba91c7159905e3f980
Auspicious <AuspiciousChan1@gmail.com>
2018-02-09 16:47:41 +0800

    mind the relationship between Image.Image and numpy.ndarray.

--- JumpPlugIn/ImageHanlder.py
+++ JumpPlugIn/ImageHanlder.py
@@ -4,7 +4,7 @@ import numpy
 from PIL import Image
 
 
-def output_gray_img_array(arr: numpy.ndarray):
+def output_ndarray_to_file(arr: numpy.ndarray):
     f0 = open("../Sources/output.txt", "w")
     f0.write("")
     f0.close()
@@ -17,9 +17,35 @@ def output_gray_img_array(arr: numpy.ndarray):
     f.close()
 
 
+# Image转换类型为ndarray
+def to_ndarry(img_gray: Image.Image)->numpy.ndarray:
+    return numpy.array(img_gray)
+
+
+# ndarray转换类型为Image
+def from_ndarray(img_arr: numpy.ndarray)->Image.Image:
+    return Image.fromarray(img_arr)
+
+
 # 返回灰度图
 def grayscale(img: Image.Image)-> Image.Image:
     return img.convert("L")
+
+
+def is_gray_img(img: Image.Image)->bool:
+    if img is None:
+        print("Not gray image: None")
+        return False
+    arr = numpy.array(img)
+    if len(arr.shape) is not 2:
+        print("Not gray image: dimension is not 2")
+        return False
+    too_big = numpy.where(arr > 255)[0]
+    too_small = numpy.where(arr < 0)[0]
+    if len(too_big) + len(too_small) > 0:
+        print("Not gray image: out of range")
+        return False
+    return True
 
 
 # 保留y方向上中间的图片
@@ -28,14 +54,18 @@ def remain_center_height(img: Image.Image)-> Image.Image:
     return img.crop((0, l/4, w, l*3/4))
 
 
-def horizontal_border(img: numpy.ndarray)-> numpy.ndarray:
-    if isinstance(img, Iterable) and isinstance(img[0], Iterable):
-        w, l = img.shape
-        img[0, :] = numpy.zeros((1, l), dtype=float)
-        for i in range(w-1):
-            img[i, :] -= img[i+1, :]
-        img = numpy.abs(img)
-    return img
+def horizontal_border(img_gray: Image.Image)-> Image.Image:
+    if is_gray_img(img_gray):
+        img_arr = to_ndarry(img_gray)
+        w, l = img_arr.shape
+        print(w, l)
+        for i in range(w - 1):
+            img_arr[i, :] -= img_arr[i+1, :]
+        img_arr[w-1, :] = numpy.zeros((1, l), dtype=int)
+        img_arr = numpy.abs(img_arr)
+        return from_ndarray(img_arr)
+    else:
+        return img_gray
 
 
 def show_ndarray_as_image(img_arr: numpy.ndarray):
@@ -43,38 +73,39 @@ def show_ndarray_as_image(img_arr: numpy.ndarray):
     img.show()
 
 
-def square_mark(img_arr: numpy.ndarray, x: int, y: int, light=255)->numpy.ndarray:
-    w, l = img_arr.shape
-    x_min = x-5 if x >= 5 else 0
-    x_max = x+5 if x < w-5 else w-1
-    y_min = y-5 if y >= 5 else 0
-    y_max = y+5 if y < l-5 else l-1
-    for i in range(x_min, x_max+1):
-        for j in range(y_min, y_max+1):
-            img_arr[i, j] = light
-    return img_arr
+# 在灰度图上绘制正方形的标记
+def mark_square(img_gray: Image.Image, x: int, y: int, light=255)->Image.Image:
+    if is_gray_img(img_gray):
+        img_arr = numpy.array(img_gray)
+        w, l = img_arr.shape
+        x_min = x - 5 if x >= 5 else 0
+        x_max = x + 5 if x < w - 5 else w - 1
+        y_min = y - 5 if y >= 5 else 0
+        y_max = y + 5 if y < l - 5 else l - 1
+        for i in range(x_min, x_max + 1):
+            for j in range(y_min, y_max + 1):
+                img_arr[i, j] = light
+        return Image.fromarray(img_arr)
+    else:
+        return img_gray
 
 
 # 获取下一个块的顶点坐标
 def get_top(img_gray: Image.Image)->tuple:
+    img_gray = horizontal_border(img_gray)
     img_arr = numpy.array(img_gray)
-    img_arr = horizontal_border(img_arr)
     xs, ys = numpy.where(img_arr > 64)
     top_index = numpy.where(xs == xs.min())
     x_top = int(numpy.average(xs[top_index]))
     y_top = int(numpy.average(ys[top_index]))
+
     return x_top, y_top
-
-
-# 判断是人头还是目标方块
-def is_head_top(img_arr: numpy.ndarray, x: int, y: int):
-    return img_arr[x+5, y] > 64
 
 
 # 获取下一个块的中点坐标
 def get_center(img_gray: Image.Image)->tuple:
+    img_gray = horizontal_border(img_gray)
     img_arr = numpy.array(img_gray)
-    img_arr = horizontal_border(img_arr)
     xs, ys = numpy.where(img_arr > 64)
     top_indexes = numpy.where(xs == xs.min())
     x_top = int(numpy.average(xs[top_indexes]))
--- JumpPlugIn/Main.py
+++ JumpPlugIn/Main.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot
 from PIL import Image
 from JumpPlugIn import ImageHanlder
 
-img: Image.Image = Image.open("../Sources/Right.png")
+img: Image.Image = Image.open("../Sources/Left.png")
 
 # 灰度图
 img_gray0 = ImageHanlder.grayscale(img)
@@ -11,17 +11,12 @@ img_gray0 = ImageHanlder.grayscale(img)
 # 去掉不重要的上下区域
 img_gray = ImageHanlder.remain_center_height(img_gray0)
 
-img_wid, img_len = img_gray.size
-
-# 图像对应的ndarray（此函数会将本来的矩阵转置后存入ndarray矩阵）
-img_arr = numpy.array(img_gray)
-img_arr.reshape((img_wid, img_len))
-
 xc, yc = ImageHanlder.get_center(img_gray)
 
-img_arr = ImageHanlder.square_mark(numpy.array(img_gray0), xc + int(img_len/2), yc, 0)
+img_gray = ImageHanlder.horizontal_border(img_gray)
 
-# img_arr = img_arr[:, 0:int(img_wid/2)] if yc > img_wid/2 else img_arr = img_arr[:, int(img_wid/2): img_wid]
+# img_gray = ImageHanlder.mark_square(img_gray, xc, yc, 0)
 
-# 此函数会将ndarray里的矩阵转置后存进图片
-ImageHanlder.show_ndarray_as_image(ImageHanlder.horizontal_border(img_arr))
+img_gray.show()
+
+ImageHanlder.output_ndarray_to_file(numpy.array(img_gray))
